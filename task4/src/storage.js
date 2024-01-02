9fa59ba739e2564a6af607fa4c93a5a248a71144
Sasha <alexak.kol.2019@gmail.com>
2024-01-02 19:35:39 +0400

    task4 ajax

--- task4/src/storage.js
+++ task4/src/storage.js
@@ -1,42 +1,60 @@
-export function setDefaultCardsInStorage() {
-    localStorage.setItem('cards', JSON.stringify(defaultArray));
-    return defaultArray;
+export async function setDefaultCardsInStorage() {
+    const savedCards = []
+    try {
+        const allCards = await (await getCardsFromStorage()).json();
+        for await (const card of allCards) {
+            await delCardFromStorage(card.id)
+        }
+        for await (const card of defaultArray) {
+            savedCards.push(await (await saveCardInStorage(card)).json())
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        return savedCards;
+    }
 }
 
 export function saveCardInStorage(card) {
     if (!card) return;
-    const cards = JSON.parse(localStorage.getItem('cards') || '[]');
-    if (!card.id) {
-        card.id = cards.length + 1;
-        cards.push(card);
-    } else {
-        const index = cards.findIndex((x) => Number(x.id) === Number(card.id));
-        cards[index] = card;
-    }
-    console.log("cards", cards);
-    localStorage.setItem('cards', JSON.stringify(cards));
-    return card;
+    
+    return fetch('http://localhost:3000/items', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(card)
+    });
+}
+
+export function updateCardInStorage(card) {
+    if (!card || !card.id) return;
+    
+    return fetch(`http://localhost:3000/items/${card.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(card)
+    });
 }
 
 export function delCardFromStorage(id) {
-    const cards = JSON.parse(localStorage.getItem('cards') || '[]');
-    localStorage.setItem('cards', JSON.stringify(cards.filter(card => card.id !== id)));
+    return fetch(`http://localhost:3000/items/${id}`, {
+        method: 'DELETE',
+    });
 }
 
 export function getCardsFromStorage() {
-    const cards = localStorage.getItem('cards');
-    if (!cards) return setDefaultCardsInStorage();
-    return JSON.parse(localStorage.getItem('cards') || '[]');
+    return fetch('http://localhost:3000/items')
 }
 
-export function getCardFromStorage(id) {
-    return JSON.parse(localStorage.getItem('cards') || '[]').find(card => card.id === id);
+export function getUserInfo() {
+    return fetch('http://localhost:3000/creatorInfo')
 }
-
 
 const defaultArray = [
     {
-        id: 1,
         img: "https://nazya.com/anyimage/ae01.alicdn.com/kf/HTB15Y7FyWmWBuNjy1Xaq6xCbXXaa/2018.jpg",
         name: "Кеды 1",
         description: "Описание описание\n описание",
@@ -44,35 +62,17 @@ const defaultArray = [
         code: 2314324
     },
     {
-        id: 2,
-        img: "https://ae01.alicdn.com/kf/HTB1vD32XOzxK1Rjy1zkq6yHrVXaQ/-.jpg",
+        img: "http://lamcdn.net/furfurmag.ru/post-cover/AmEAJrRXAnJlkIFoC7ahfg-default.jpg",
         name: "Кеды 2",
         description: "Описание описание\n описание",
         provider: "Поставщик 2",
-        code: 1223432
+        code: 987675
     },
     {
-        id: 3,
-        img: "https://ae01.alicdn.com/kf/HTB1QPGVcy6guuRkSmLyq6AulFXa3/Canvas-Children-Sport-Flat-Shoes-Breathable-Boys-Girls-Sneakers-Kids-Flats-Shoes-for-Girls-Jeans-Denim.jpg",
+        img: "https://ae04.alicdn.com/kf/H1ef3094fe1d640da8d5f385a563f23ddT.jpg",
         name: "Кеды 3",
         description: "Описание описание\n описание",
         provider: "Поставщик 3",
-        code: 5435436
-    },
-    {
-        id: 4,
-        img: "http://lamcdn.net/furfurmag.ru/post-cover/AmEAJrRXAnJlkIFoC7ahfg-default.jpg",
-        name: "Кеды 4",
-        description: "Описание описание\n описание",
-        provider: "Поставщик 4",
-        code: 43276765
-    },
-    {
-        id: 5,
-        img: "https://ae04.alicdn.com/kf/H1ef3094fe1d640da8d5f385a563f23ddT.jpg",
-        name: "Кеды 5",
-        description: "Описание описание\n описание",
-        provider: "Поставщик 5",
-        code: 4534534
-    },
+        code: 58566
+    }
 ]
